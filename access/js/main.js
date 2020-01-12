90b3eceeaa8297d9888e9e9b16b960f1649b8fa6
cucurbiturilum <cucurbiturilum@gmail.com>
2020-01-12 02:43:31 +0200

    ended private account page

--- access/js/main.js
+++ access/js/main.js
@@ -9,4 +9,32 @@ $(document).ready(function () {
     var countdown = 60;
     $('svg circle').attr('style', 'animation:countdown ' + countdown + 's linear infinite forwards;')
 
+
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),
+        x = setInterval(function() {
+
+            let now = new Date().getTime(),
+                distance = countDown - now;
+
+            var classToValue = {
+                days: Math.floor(distance / (day)),
+                hours: Math.floor((distance % (day)) / (hour)),
+                minutes: Math.floor((distance % (hour)) / (minute)),
+                seconds: Math.floor((distance % (minute)) / second)
+            };
+
+            Object.keys(classToValue).forEach(function(className){
+                var value = classToValue[className];
+                var elements = document.getElementsByClassName(className);
+                for(var element of elements){
+                    element.innerText = value;
+                }
+            });
+
+        }, second)
 });
