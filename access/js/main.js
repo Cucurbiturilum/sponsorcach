c5f10b758f172027a817f165ba2f6608c3c9f807
cucurbiturilum <cucurbiturilum@gmail.com>
2020-01-13 03:40:46 +0200

    finished project page

--- access/js/main.js
+++ access/js/main.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+// Likes
+
     var toggleLike = $('.like');
     var heart = $('.fa-heart');
 
@@ -6,9 +9,12 @@ $(document).ready(function () {
         heart.toggleClass('far fa-heart fas fa-heart');
     });
 
+// Circle Timer
+
     var countdown = 60;
     $('svg circle').attr('style', 'animation:countdown ' + countdown + 's linear infinite forwards;')
 
+// Timer
 
     const second = 1000,
         minute = second * 60,
@@ -16,7 +22,7 @@ $(document).ready(function () {
         day = hour * 24;
 
     let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),
-        x = setInterval(function() {
+        x = setInterval(function () {
 
             let now = new Date().getTime(),
                 distance = countDown - now;
@@ -28,13 +34,57 @@ $(document).ready(function () {
                 seconds: Math.floor((distance % (minute)) / second)
             };
 
-            Object.keys(classToValue).forEach(function(className){
+            Object.keys(classToValue).forEach(function (className) {
                 var value = classToValue[className];
                 var elements = document.getElementsByClassName(className);
-                for(var element of elements){
+                for (var element of elements) {
                     element.innerText = value;
                 }
             });
 
         }, second)
+//Project
+
+    var projectDetailsBtn = $('#details');
+    var sponsorsBtn = $('#sponsors');
+    var feedbackBtn = $('#feedback');
+
+    var projectDetailsContent = $('.project-details');
+    var sponsorsContent = $('.sponsors');
+    var feedbackContent = $('.feedback');
+
+    projectDetailsContent.show();
+    sponsorsContent.hide();
+    feedbackContent.hide();
+
+    projectDetailsBtn.click(function(){
+        projectDetailsContent.show();
+        sponsorsContent.hide();
+        feedbackContent.hide();
+
+        projectDetailsBtn.addClass('active');
+        sponsorsBtn.removeClass('active')
+        feedbackBtn.removeClass('active');
+    });
+
+    sponsorsBtn.click(function(){
+        projectDetailsContent.hide();
+        sponsorsContent.show();
+        feedbackContent.hide();
+
+        projectDetailsBtn.removeClass('active');
+        sponsorsBtn.addClass('active')
+        feedbackBtn.removeClass('active');
+    });
+
+    feedbackBtn.click(function(){
+        projectDetailsContent.hide();
+        sponsorsContent.hide();
+        feedbackContent.show();
+
+        projectDetailsBtn.removeClass('active');
+        sponsorsBtn.removeClass('active')
+        feedbackBtn.addClass('active');
+    });
+
 });
